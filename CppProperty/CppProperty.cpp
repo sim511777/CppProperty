0482a8905a9d9e082fb6bd14b45b75cd445fc055
sim511777 <sim51177@gmail.com>
2021-08-30 10:36:27 +0900

    초기 커밋.

---
+++ CppProperty/CppProperty.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include "Property.h"
+
+class Person {
+private:
+    // 내부 변수
+    int _age;
+public:
+    // property
+    property<int> age {
+        [this](int value) { // setter
+            this->_age = value;
+        },
+        [this]() -> int {   // getter
+            return this->_age;
+        },
+    };
+};
+
+int main() {
+    Person p;
+    p.age = 10;
+    std::cout << p.age;
+}
---
+++ CppProperty/Property.h
@@ -0,0 +1,20 @@
+#pragma once
+#include <functional>
+
+template<typename T>
+class property {
+private:
+    std::function<void(T)> _setter;
+    std::function<T()> _getter;
+public:
+    property(std::function<void(T)> setter, std::function<T()> getter)
+        : _setter(setter), _getter(getter) {
+    }
+    operator T() const {
+        return _getter();
+    }
+    property<T>& operator= (const T &value) {
+        _setter(value);
+        return *this;
+    }
+};
